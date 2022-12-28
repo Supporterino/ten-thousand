2810567607c99f2bb30b9fd27cfbc8c58100ce4a
Supporterino <lars@roth-kl.de>
2022-12-28 18:14:16 +0100

    feat(client): :sparkles: Add SocketManager and prettier

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,15 +1,26 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+import SocketManager from '@components/websocket/SocketManager';
+import { useSetRecoilState } from 'recoil';
+import SocketState from '@components/websocket/SocketState';
+import { createContext } from 'react';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+  const socketManager = new SocketManager();
+  const SocketManagerContext = createContext(socketManager);
+  socketManager.setSocketState = useSetRecoilState(SocketState);
 
   return (
     <>
       <Head>
         <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
 
       <MantineProvider
@@ -20,7 +31,11 @@ export default function App(props: AppProps) {
           colorScheme: 'dark',
         }}
       >
-        <Component {...pageProps} />
+        <NotificationsProvider position="bottom-center" limit={15}>
+          <SocketManagerContext.Provider value={socketManager}>
+            <Component {...pageProps} />
+          </SocketManagerContext.Provider>
+        </NotificationsProvider>
       </MantineProvider>
     </>
   );
