b4c2d9e71af2f89147d00be7285fd61ba9d5cee9
Supporterino <lars@roth-kl.de>
2022-12-28 18:34:51 +0100

    feat(shared): :sparkles: Add Events for Notification and Lobby update

--- packages/shared/server/ServerEvents.ts
+++ packages/shared/server/ServerEvents.ts
@@ -1,5 +1,7 @@
 const enum ServerEvents {
   Pong = 'server.pong',
+  LobbyState = 'server.lobby.state',
+  GameNotification = 'server.notification',
 }
 
 export { ServerEvents };
--- packages/shared/server/ServerPayloads.ts
+++ packages/shared/server/ServerPayloads.ts
@@ -4,6 +4,15 @@ type ServerPayloads = {
   [ServerEvents.Pong]: {
     message: string;
   };
+
+  [ServerEvents.GameNotification]: {
+    message: string;
+    color: string;
+  };
+
+  [ServerEvents.LobbyState]: {
+    lobbyId: string;
+  };
 };
 
 export { ServerPayloads };
