81ac71e8583b3c201475a9fa7f7e7d3fbf61245b
Supporterino <lars@roth-kl.de>
2022-12-28 18:58:16 +0100

    fix(client): :bug: Fix tailwind not working

--- packages/client/components/ui/Header.tsx
+++ packages/client/components/ui/Header.tsx
@@ -1,7 +1,7 @@
 const Header: React.FunctionComponent = () => {
   return (
-    <div className="w-full mx-2">
-      <h1 className="flex space-x-2 text-4x1 font-bold mx-2 underline">
+    <div>
+      <h1 className="underline">
         <span>Ten thousand</span>
       </h1>
     </div>
