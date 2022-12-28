3d65d1900b338a5f9a6b69756f3270c1e2ca1c70
Supporterino <lars@roth-kl.de>
2022-12-28 17:14:28 +0100

    feat(client): :sparkles: Add tailwind css

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,6 +1,11 @@
+// Need to transpile typescript from shared workspace
+const withTM = require('next-transpile-modules')([
+  '@the-ten-thousand/shared',
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig)
