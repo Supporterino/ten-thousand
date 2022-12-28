fe8014adac3cb43294c336152aa3343aa24b12ac
Supporterino <lars@roth-kl.de>
2022-12-28 15:52:11 +0100

    feat(server): :sparkles: Add basic lobby construct

---
+++ packages/server/src/game/lobby/lobby.ts
@@ -0,0 +1,36 @@
+import { v4 } from 'uuid';
+import { Server, Socket } from 'socket.io';
+import { AuthenticatedSocket } from '../types';
+import { ServerEvents } from '@app/../shared/server/ServerEvents';
+
+class Lobby {
+  public readonly id: string = v4();
+  public readonly createdAt: Date = new Date();
+  public readonly clients: Map<Socket['id'], AuthenticatedSocket> = new Map<
+    Socket['id'],
+    AuthenticatedSocket
+  >();
+
+  constructor(
+    private readonly server: Server,
+    public readonly numberOfClients: number,
+  ) {}
+
+  public addClient(client: AuthenticatedSocket): void {
+    this.clients.set(client.id, client);
+    client.join(this.id);
+    client.data.lobby = this;
+  }
+
+  public removeClient(client: AuthenticatedSocket): void {
+    this.clients.delete(client.id);
+    client.leave(this.id);
+    client.data.lobby = null;
+  }
+
+  public dispatchToLobby<T>(event: ServerEvents, payload: T): void {
+    this.server.to(this.id).emit(event, payload);
+  }
+}
+
+export default Lobby;
--- packages/server/src/game/types.ts
+++ packages/server/src/game/types.ts
@@ -1,9 +1,10 @@
 import { ServerEvents } from '@shared/server/ServerEvents';
 import { Socket } from 'socket.io';
+import Lobby from './lobby/lobby';
 
 export type AuthenticatedSocket = Socket & {
   data: {
-    lobby: null;
+    lobby: null | Lobby;
   };
 
   emit: <T>(ev: ServerEvents, data: T) => boolean;
