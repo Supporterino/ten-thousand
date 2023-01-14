0471a6dc853969bcea6527b05fcac5e1152fbf12
Supporterino <lars@roth-kl.de>
2023-01-14 14:40:47 +0100

    feat(client): :sparkles: Add PWA capablitilies

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,9 +1,12 @@
 // Need to transpile typescript from shared workspace
 const withTM = require('next-transpile-modules')(['@the-ten-thousand/shared']);
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withTM(nextConfig);
+module.exports = withPWA(withTM(nextConfig));
